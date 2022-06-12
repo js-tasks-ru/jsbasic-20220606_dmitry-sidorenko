0a6a391dd33c5362343a3ee93b09aeeb5f7f8c62
Dmitry Sidorenko <dsidorenko@intermedia.com>
2022-06-12 20:28:11 +0300

    1st module, task 2 refactoring

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,7 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  return !!name && (name?.length > 3 && !name?.includes(" "));
+  return !!name && name.length > 3 && !name.includes(" ");
 }
 
 function sayHello() {
