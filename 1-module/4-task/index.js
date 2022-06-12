e34e4748ab13a03b7a256497dc3cbe17c718cf0c
Dmitry Sidorenko <dsidorenko@intermedia.com>
2022-06-12 20:47:36 +0300

    1st module, task 4

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,6 @@
 function checkSpam(str) {
-  // ваш код...
+  spamFingerPrint1 = '1xBet'.toLowerCase();
+  spamFingerPrint2 = 'XXX'.toLowerCase();
+
+  return str.toLowerCase().includes(spamFingerPrint1) || str.toLowerCase().includes(spamFingerPrint2);
 }
