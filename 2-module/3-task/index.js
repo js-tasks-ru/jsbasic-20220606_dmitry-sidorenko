c3ddc4c65d703d48f3e4c5b421bc90c16cbb6c7e
Dmitry Sidorenko <dsidorenko@intermedia.com>
2022-06-13 21:24:12 +0300

    2nd module, task 3

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,14 @@
 let calculator = {
-  // ваш код
+  read: function(a, b) {
+    this.left = a;
+    this.right = b;
+  },
+  sum: function() {
+    return this.left + this.right;
+  },
+  mul: function() {
+    return this.left * this.right;
+  },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
