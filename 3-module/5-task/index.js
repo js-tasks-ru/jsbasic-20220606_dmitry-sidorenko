78230161ebac69b95eac2d7a057934e987407c12
Dmitry Sidorenko <dsidorenko@intermedia.com>
2022-06-16 22:35:39 +0300

    3rd module, task 5

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,10 @@
 function getMinMax(str) {
-  // ваш код...
+  let numbers = str
+                  .split(" ")
+                  .map(elem => +elem)
+                  .filter(elem => isFinite(elem));
+  return {
+    min: Math.min(...numbers),
+    max: Math.max(...numbers),
+  };
 }
