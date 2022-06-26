3aae103d43ba25f3defdb6db28952924b5126190
Dmitry Sidorenko <dsidorenko@intermedia.com>
2022-06-26 16:52:35 +0300

    5th module, task 3

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,41 @@
 function initCarousel() {
-  // ваш код...
+  const numberOfSlides = 4;
+  const leftBtn = document.querySelector('.carousel__arrow_left');
+  const rightBtn = document.querySelector('.carousel__arrow_right');
+  let innerCarousel = document.querySelector('.carousel__inner');
+  const slideWidth = innerCarousel.offsetWidth;
+  let offset = 0;
+
+  class CarouselButtonHandler {
+
+    handleEvent(event) {
+      leftBtn.style.display = '';
+      rightBtn.style.display = '';
+
+      switch (event.currentTarget) {
+      case leftBtn:
+        offset += slideWidth;
+        innerCarousel.style.transform = `translateX(${offset}px)`;
+        break;
+      case rightBtn:
+        offset -= slideWidth;
+        innerCarousel.style.transform = `translateX(${offset}px)`;
+        break;
+      }
+
+      if (offset === 0) {
+        leftBtn.style.display = 'none';
+      }
+      if (offset === -slideWidth * (numberOfSlides - 1)) {
+        rightBtn.style.display = 'none';
+      }
+    }
+  }
+
+  let btnHandler = new CarouselButtonHandler();
+
+  leftBtn.addEventListener('click', btnHandler);
+  rightBtn.addEventListener('click', btnHandler);
+
+  leftBtn.style.display = 'none';
 }
