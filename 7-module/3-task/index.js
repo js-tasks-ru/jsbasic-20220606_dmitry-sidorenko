ff0aff21408fc75365f122f0109ef2d0907a1640
Dmitry Sidorenko <dsidorenko@intermedia.com>
2022-07-11 23:43:59 +0300

    7th module, task 4

--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -3,6 +3,7 @@ import createElement from '../../assets/lib/create-element.js';
 export default class StepSlider {
   #elem;
   #sliderSegmentWidth = 0;
+
   constructor({ steps, value = 0 }) {
     this.#elem = createElement(`
       <div class="slider">
